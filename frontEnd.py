45f644f053a704482f38a26d8b940a09cf426b2c
Adrian <diazmezaadrian@gmail.com>
2012-05-15 13:07:59 -0600

    frontEnd final

--- frontEnd.py
+++ frontEnd.py
@@ -1,15 +1,21 @@
 #!/usr/local/bin/python
 # -*- coding: latin-1 -*-
-                 # Permite abrir archivos dentro de la carpeta del programa
+
+#funciones del archivo backEnd
 from backEnd import insertar
 from backEnd import consulta
+
+#librerias Importadas
 import Tkinter
 from Tkinter import *  
 import ttk
+import tkMessageBox
 
+#se define la ventana principal
 ventana =Tk()
 ventana.title("Zoo")
 
+#pestañas de la ventana principal
 notebook = ttk.Notebook(ventana)
 notebook.pack(fill='both', expand='yes')
 
@@ -19,6 +25,7 @@ frame2 = ttk.Frame(notebook)
 notebook.add(frame1, text='Inserccion')
 notebook.add(frame2, text='Consultas')
 
+#funcion de ingreso enviar e insertar datos en el backend
 def Ingreso():
 	raza=eraza.get()
 	nombre=enombre.get()
@@ -26,8 +33,13 @@ def Ingreso():
 	edad=eedad.get()
 	ecosistema=eecosistema.get()
 	comida=ecomida.get()
-	insertar(raza,nombre,genero,edad,ecosistema,comida)
+	if (eraza.get()=="" or enombre.get()=="" or eedad.get()=="" or eecosistema.get()=="" or egenero.get()=="" or ecomida.get()==""):
+		Error()
+	else:
+		insertar(raza,nombre,genero,edad,ecosistema,comida)
+		Aviso()
 
+#funcion de consulta para enviar y recibir una respuesta del backend
 def Consulta():
 	raza=craza.get()
 	nombre=cnombre.get()
@@ -35,6 +47,7 @@ def Consulta():
 	edad=cedad.get()
 	ecosistema=cecosistema.get()
 	comida=ccomida.get()
+	
 	if craza.get()=="":
 		raza="vacio"
 	if cnombre.get()=="":
@@ -47,12 +60,43 @@ def Consulta():
 		ecosistema="vacio"
 	if ccomida.get()=="":
 		comida="vacio"
-		
-	#print(raza,nombre,genero,edad,ecosistema,comida)
-	consulta(raza,nombre,genero,edad,ecosistema,comida)
-	
-def Salida():#FUNCION DE SALIDA DEL PROGRAMA    
+	#llama a la funcion consulta del backend	
+	lista=consulta(raza,nombre,genero,edad,ecosistema,comida)
+	Ventana(lista)
+
+#funcion de salida del programa
+def Salida():
     ventana.destroy()
+ 
+ #mensaje de error al intentar insertar datos vacios
+def Error():
+	tkMessageBox.showinfo(title="Error", \
+    message="Tiene que llenar todos los atributos del animal")
+
+#funcion de aviso al insertar los datos
+def Aviso():
+	tkMessageBox.showinfo(title="Aviso", \
+    message="Insercción Exitosa")
+    
+#ventana emergente con los resultados de las consultas
+def Ventana(lista):
+	ventana2 = Tk()
+	ventana2.title("Consulta:")
+	plano_3 = Label(ventana2)
+	plano_3.pack()
+	
+	label_15 = Label(plano_3,text="Resultado: ", font = ("Helvetica", 16), fg = "black")
+	label_15.place(x = 10, y = 2)
+	label_15.pack()
+	
+	for i in range(len(lista)):
+			label_15 = Label(plano_3,text= ("--Raza:",lista[i][0],"--Nombre:",lista[i][1],"--Genero:",lista[i][2]
+			,"--Edad:",lista[i][3],"--Ecosistema:",lista[i][4],"--Comida:",lista[i][5]), font = ("Helvetica", 14), fg = "black")
+			label_15.place(x = 0, y = 50)
+			label_15.pack()
+	
+	ventana2.geometry('800x300')
+	ventana2.mainloop()
 
 #botones
 plano_1 = Button(frame1, text = "Agregar Animales", font = ("Helvetica", 10), command=Ingreso)
@@ -67,7 +111,6 @@ boton_1.place(x = 150, y = 220)
 boton_1 = Button(frame2, text = "Salir",font = ("Helvetica", 10), command = Salida)
 boton_1.place(x = 150, y = 220)
 
-#-----------------------------------------------------------------------------------------------------------
 #En este bloque se encuentran los labels de la parte de Ingreso
 
 label_1 = Label(frame1,text="Ingrese los datos del animal: ", font = ("Helvetica", 16), fg = "black")
@@ -91,7 +134,6 @@ label_6.place(x = 10, y = 160)
 label_7 = Label (frame1,text="Comida Favorita: ", font = ("Helvetica", 12), fg = "black")
 label_7.place(x = 10, y = 190)
 
-#---------------------------------------------------------------------------------------------------------
 nombre = StringVar()
 enombre = Entry(frame1, textvariable = nombre)
 enombre.place(x = 145, y = 40)
@@ -116,7 +158,6 @@ comida = StringVar()
 ecomida = Entry(frame1, textvariable = comida)
 ecomida.place(x = 145, y = 190)
 
-#-----------------------------------------------------------------------------------------------------------
 #En este bloque se encuentran los labels de la parte de Consulta
 
 label_8 = Label(frame2,text="Ingrese los datos del animal: ", font = ("Helvetica", 16), fg = "black")
@@ -140,7 +181,6 @@ label_13.place(x = 10, y = 160)
 label_14 = Label (frame2,text="Comida Favorita: ", font = ("Helvetica", 12), fg = "black")
 label_14.place(x = 10, y = 190)
 
-#---------------------------------------------------------------------------------------------------------
 nombre = StringVar()
 cnombre = Entry(frame2, textvariable = nombre)
 cnombre.place(x = 145, y = 40)
@@ -164,8 +204,6 @@ cecosistema.place(x = 145, y = 160)
 comida = StringVar()
 ccomida = Entry(frame2, textvariable = comida)
 ccomida.place(x = 145, y = 190)
-
-#barra.pack(side=TOP, fill=X)
 
 ventana.geometry('350x300')
 ventana.mainloop()
